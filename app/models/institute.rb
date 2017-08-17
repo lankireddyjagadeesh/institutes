9599d27d59332c9fe644579709075493b41e70d3
lankireddyjagadeesh <l.jagadeesh1991@gmail.com>
2017-08-17 15:58:26 +0530

    created

--- app/models/institute.rb
+++ app/models/institute.rb
@@ -2,6 +2,7 @@ class Institute < ActiveRecord::Base
 	has_and_belongs_to_many :courses
 	validates :name, :address,:contact, :mailid, :presence => true
 	validates_format_of :mailid, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	
 
 
 end
